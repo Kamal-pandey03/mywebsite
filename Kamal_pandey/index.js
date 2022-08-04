97ac0dfa468bf82920461dad739a306e372588e0
Kamal-pandey03 <kamal.201712@ncit.edu.np>
2022-08-04 23:29:39 +0545

    hehe

---
+++ Kamal_pandey/index.js
@@ -0,0 +1,15 @@
+var appleFruit = document.getElementById("apple");
+function dropapple(event){
+    // var appleFruit = $("<img class='apple'/>");
+    // appleFruit.attr("src","https://toppng.com//public/uploads/preview/apple-fruit-png-apple-fruit-transparent-11563073201mj52xnr1gp.png");
+    // appleFruit.style.left = event.clientX + "px";
+    // appleFruit.style.top = event.clientY + "px";
+    // appleFruit.classList.add('showApple');
+    appleFruit.style.display = "block";
+    appleFruit.style.top=event.clientY+"px";
+    appleFruit.style.left=event.clientX+"px";
+    appleFruit.style.zIndex="99";
+
+    event.stopPropagation();
+  event.preventDefault();
+}
