d44ce548d3bfd47151d6d5432146d7adf24e1746
Kamal-pandey03 <kamal.201712@ncit.edu.np>
2022-08-05 18:03:52 +0545

    updates

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var appleFruit = document.getElementById("apple");
 function dropapple(event){
-    // var appleFruit = $("<img class='apple'/>");
-    // appleFruit.attr("src","https://toppng.com//public/uploads/preview/apple-fruit-png-apple-fruit-transparent-11563073201mj52xnr1gp.png");
-    // appleFruit.style.left = event.clientX + "px";
-    // appleFruit.style.top = event.clientY + "px";
-    // appleFruit.classList.add('showApple');
-    appleFruit.style.display = "block";
-    appleFruit.style.top=event.clientY+"px";
-    appleFruit.style.left=event.clientX+"px";
-    appleFruit.style.zIndex="99";
+  // var appleFruit = $("<img class='apple'/>");
+  // appleFruit.attr("src","https://toppng.com//public/uploads/preview/apple-fruit-png-apple-fruit-transparent-11563073201mj52xnr1gp.png");
+  // appleFruit.style.left = event.clientX + "px";
+  // appleFruit.style.top = event.clientY + "px";
+  appleFruit.classList.remove('apple');
+  appleFruit.classList.add('dropapple');
 
-    event.stopPropagation();
-  event.preventDefault();
+//   appleFruit.style.display = "block";
+//   appleFruit.style.top=event.clientY+"px";
+//   appleFruit.style.left=event.clientX+"px";
+//   appleFruit.style.zIndex="99";
+
+//   event.stopPropagation();
+// event.preventDefault();
 }
